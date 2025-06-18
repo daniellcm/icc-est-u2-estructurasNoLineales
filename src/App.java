1ee10a0847cb062fce72b635ec7282e144ec73bc
Paul-Astudillo <pastudilloc1@est.ups.edu.ec>
2025-06-17 19:42:39 -0500

    Estrucuturas no lineales

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import materia.controllers.BinaryTree;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BinaryTree ab = new BinaryTree();
+        ab.insert(50);
+        ab.insert(17);
+        ab.insert(76);
+        ab.insert(9);
+        ab.insert(23);
+        ab.insert(54);
+        ab.insert(14);
+        ab.insert(19);
+        ab.imprimirArbol();
+        System.out.println();
+        System.out.println("PosOrden: ");
+        ab.printPosOrden();
+        System.out.println();
+        System.out.println("InOrden: ");
+        ab.printInOrden();
+        System.out.println();
     }
 }
---
+++ src/materia/controllers/BinaryTree.java
@@ -0,0 +1,72 @@
+package materia.controllers;
+import materia.models.Node;
+
+public class BinaryTree {
+    
+    private Node root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public void insert(int value) { //17
+                 //N(50), 17
+        root = insertRec(root, value);
+    }
+
+    private Node insertRec(Node padre, int value){
+                //N(50), 17 
+        if(padre == null) {
+            return new Node(value);
+        }   
+
+        if (value <= padre.getValue()) {
+            //Me voy a la izquierda
+            Node newNode = insertRec(padre.getLeft(), value);
+            padre.setLeft(newNode);
+        } else if (value > padre.getValue()){
+            //Me voy a la derecha
+            padre.setRight(insertRec(padre.getRight(), value));
+        }
+
+        return padre;
+    }
+
+    public void imprimirArbol() {
+        imprimirArbolRec(root);
+    }
+
+    private void imprimirArbolRec(Node node) {
+        if (node != null) {
+            
+            System.out.print(node.getValue() + ",");
+            imprimirArbolRec(node.getLeft());
+            imprimirArbolRec(node.getRight());
+        }
+    }
+
+    public void printPosOrden() {
+        printPosOrdenRec(root);
+    }
+
+    private void printPosOrdenRec(Node node) {
+        if (node != null) {
+            printPosOrdenRec(node.getLeft());
+            printPosOrdenRec(node.getRight());
+            System.out.print(node.getValue() + ",");
+        }
+    }
+
+    public void printInOrden() {
+        printInOrdenRec(root);
+    }
+
+    private void printInOrdenRec(Node node) {
+        if (node != null) {
+            printInOrdenRec(node.getLeft());
+            System.out.print(node.getValue() + ",");
+            printInOrdenRec(node.getRight());
+        }
+    }
+
+}
